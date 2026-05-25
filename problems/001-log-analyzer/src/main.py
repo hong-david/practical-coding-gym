9772762d1027f11f2008cd420cf507b3e0e3e485
David Hong <davidhong.9899@gmail.com>
2026-05-24 21:34:42 -0400

    basic MVP for processing log lines

--- problems/001-log-analyzer/src/main.py
+++ problems/001-log-analyzer/src/main.py
@@ -1,6 +1,17 @@
-def main():
-    raise NotImplementedError
+from log_analyzer import analyze_log_file
+import sys
 
+def main():
+    if len(sys.argv) != 2:
+        print('Error: requires 2 arguments')
+        sys.exit(1)
+    try:
+        filepath = sys.argv[1]
+        analyzedLogFile = analyze_log_file(filepath)
+        print(analyzedLogFile)
+    except FileNotFoundError as error:
+        print(f'Error: {error}', file=sys.stderr)
+        sys.exit(1)
 
 if __name__ == "__main__":
     main()
